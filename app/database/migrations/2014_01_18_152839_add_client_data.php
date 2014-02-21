2e3239e0abbe80ed4ae8aff8ccf438a7920db059
redsh1ftr <dev@precisemaintenance.com>
2014-02-20 20:48:15 -0500

    Added stuff
    Timestamp now on call report -- added office manager field to client
    table.

--- app/database/migrations/2014_01_18_152839_add_client_data.php
+++ app/database/migrations/2014_01_18_152839_add_client_data.php
@@ -15,6 +15,7 @@ class AddClientData extends Migration {
 		DB::table('client_list')->insert(array(
 				'client_id' => '4050',
 				'business_name' => 'The Electric Company',
+				'office_manager' => 'manager of the office',
 				'net_terms' => '30',
 				'work_order_number' => '014',
 				'hours' => 'Monday - Friday 9-5, Saturday 10-3'
@@ -24,6 +25,7 @@ class AddClientData extends Migration {
 		DB::table('client_list')->insert(array(
 				'client_id' => '4051',
 				'business_name' => 'The Plumbing Company',
+				'office_manager' => 'manager',
 				'net_terms' => '30',
 				'work_order_number' => '009',
 				'hours' => 'Monday - Friday 9-5, Saturday 10-3'
